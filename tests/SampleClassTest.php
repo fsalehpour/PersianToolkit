896e10c7dc2a86e84e056f15c8d1f7fde19cace2
Faramarz Salehpour <subs@marz.co>
2017-03-07 12:27:17 +0330

    Add tests folder with a sample Test

---
+++ tests/SampleClassTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Faramarz Salehpour
+ */
+
+
+use PHPUnit\Framework\TestCase;
+
+class SampleClassTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itReturnsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
